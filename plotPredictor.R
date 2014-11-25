53f497e54fbe662c1baf823b0cc272ff5caf1188
Daiqian <dz2203@columbia.edu>
2014-11-24 23:28:28 -0500

    deleted comments

--- plotPredictor.R
+++ plotPredictor.R
@@ -1,16 +1,9 @@
 source("import.R") 
 
-## featurePlot(x = tt[,c("Sex","Age","Parch")],
-##             y = tt$Survived,
-##             plot = "pairs")
-
 outcome.name = "Survived"
 
 for (head in colnames(tt)) {
   if(head != outcome.name) {
-
-    counts <- table(tt[,outcome.name], tt[,head])
-
 
 		qq <- ggplot(tt, aes(x = tt[,head])) + xlab(head) + geom_bar(aes(fill = factor(tt[,outcome.name])))
 		print(qq)
@@ -19,10 +12,3 @@ for (head in colnames(tt)) {
 		print(qq)
   }
 }
-
-## counts <- table(mtcars$vs, mtcars$gear)
-## barplot(counts, main="Car Distribution by Gears and VS",
-##         xlab="Number of Gears", col=c("darkblue","red"),
-##         legend = rownames(counts)) 
-
-## plot(Age,Survived,data=tt)
