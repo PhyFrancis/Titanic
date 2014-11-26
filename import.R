452cd95d84ad13f7a04c25c5db0f0f59d415a8ee
Daiqian <dz2203@columbia.edu>
2014-11-26 01:07:19 -0500

    added graphs

--- import.R
+++ import.R
@@ -1,6 +1,4 @@
-library(caret);
-
-tt = read.csv("data/train.csv")
+df = read.csv("data/train.csv")
 
 ## TODO 
 ## imputing data or not?
--- plotPredictor.R
+++ plotPredictor.R
@@ -1,14 +1,33 @@
 source("import.R") 
 
+library(caret);
+library(Amelia);
+
 outcome.name = "Survived"
 
-for (head in colnames(tt)) {
+sink("result/summary.txt")
+summary(df);
+
+pdf("result/Rplot.pdf")
+missmap(df);
+for (head in colnames(df)) {
   if(head != outcome.name) {
 
-		qq <- ggplot(tt, aes(x = tt[,head])) + xlab(head) + geom_bar(aes(fill = factor(tt[,outcome.name])))
+		qq <- ggplot(df, aes(x = df[,head])) + xlab(head) + geom_bar(aes(fill = factor(df[,outcome.name])))
 		print(qq)
 
-		qq <- ggplot(tt, aes(x = tt[,head])) + xlab(head) + geom_bar(aes(fill = factor(tt[,outcome.name])), position = 'fill')
+		qq <- ggplot(df, aes(x = df[,head])) + xlab(head) + geom_bar(aes(fill = factor(df[,outcome.name])), position = 'fill')
 		print(qq)
   }
+}
+
+exclude <- c("Name")
+heads <- colnames(df)
+for (head1 in heads) {
+	if(head1 == outcome.name | head1 %in% exclude) next
+	for (head2 in heads) {
+		if(head2 == outcome.name | head2 %in% exclude | head2 == head1) next
+		if(match(head1,heads) <= match(head2,heads)) next
+		print(qplot(df[,head1],df[,head2],colour=Survived,data=df) + xlab(head1) + ylab(head2))
+	}
 }
