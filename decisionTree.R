b98b271b817adf87387b52a3826baa07b043cef3
Daiqian <dz2203@columbia.edu>
2014-11-26 23:09:23 -0500

    add bagging procedure

--- decisionTree.R
+++ decisionTree.R
@@ -1,19 +1,33 @@
-source("import.R") 
+source("resample.R") 
 
 library(caret);
 
 ## train the model
-modFit <- train(Survived ~ Sex + Pclass + SibSp + Parch + Fare + Embarked, 
-                method="rpart",data=training);
+modFit <- list();
+for(i in 1:length(folds)) {
+	modFit[[i]] <- train(Survived ~ Sex + Pclass + SibSp + Parch + Fare + Embarked, 
+ 									method="rpart",
+									data = training[folds[[i]],]);
+}
 
 ## print out model
-print(modFit$finalModel)
-pdf("data/tree.pdf")
-plot(modFit$finalModel, uniform = TRUE,
-     main = "Classification Tree")
-text(modFit$finalModel, use.n = TRUE, all = TRUE, cex = .8)
+pdf("data/tree.pdf");
+for(i in 1:length(folds)) {
+	print(modFit[[i]]$finalModel);
+	plot(modFit[[i]]$finalModel, uniform = TRUE,
+			 main = "Classification Tree");
+	text(modFit[[i]]$finalModel, use.n = TRUE, all = TRUE, cex = .8);
+}
 
 ## predict
-pred <- as.integer(predict(modFit,newdata=testing) + 0.5)
+pred = 0;
+for(i in 1:length(folds)) {
+	pred <- pred + predict(modFit[[1]],newdata=testing);
+}
+pred <- as.integer(pred / length(folds) + 0.5);
 obs <- testing$Survived
 confusionMatrix(obs, pred)
+
+## pred <- as.integer(predict(modFit[[1]],newdata=testing) + 0.5)
+## obs <- testing$Survived
+## confusionMatrix(obs, pred)
