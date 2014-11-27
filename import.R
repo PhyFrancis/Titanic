b748e1114b6ab18cb66d44bf34bbbface6416e2b
Daiqian <dz2203@columbia.edu>
2014-11-27 00:58:36 -0500

    trying to impute missing age

--- import.R
+++ import.R
@@ -1,15 +1,24 @@
 library(caret)
 
-goal = "Survived"
-df = read.csv("data/train.csv")
+goal <- "Survived"
+df <- read.csv("data/train.csv")
 
-#set.seed(123432)
-set.seed(31415)
+## imputing/dropping data 
+df$Cabin <- NULL ## too many missing in Cabin
+# for(i in 1:length(df$Name)) {
+# 	for(title in c("Mr","Mrs","Miss")) {
+# 		if(grepl(title,df$Name[i])) {
+# 			df$Name[i] <- title;
+# 			break
+# 		}
+# 	}
+# }
+# summary(df$Name)
+
+## partition data
+set.seed(123432)
 inTrain <- createDataPartition(df[,goal],
-                               p = 0.75,
+                               p = 0.8,
                                list = FALSE)
 training <- df[inTrain,]
 testing <- df[-inTrain,]
-
-## imputing data 
-#preProcess(training$Age, method="BoxCox")
